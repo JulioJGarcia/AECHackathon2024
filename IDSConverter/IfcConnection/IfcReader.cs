e6106c20e7afe1aa76041691ad4cbe60823bcd71
Julio J Garcia <julio@modelical.com>
2024-02-03 20:07:23 +0100

    commit for merge

--- IDSConverter/IfcConnection/IfcReader.cs
+++ IDSConverter/IfcConnection/IfcReader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IDSConverter.Items;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,79 @@ namespace IDSConverter.IfcConnection
             File = file;
         }
 
+        private IDS ConvertToXmlFormat(List<Dictionary<string, string>> excelData)
+        {
+            IDS specs = new IDS()
+            {
+                Xmlns = "http://standards.buildingsmart.org/IDS",
+                XmlnsXs = "http://www.w3.org/2001/XMLSchema",
+                XmlnsXsi = "http://www.w3.org/2001/XMLSchema-instance",
+                XsiSchemaLocation = "http://standards.buildingsmart.org/IDS ids.xsd",
+                Specifications = new List<Specification>()
+            };
+
+            foreach (Dictionary<string, string> row in excelData)
+            {
+                if (excelData.IndexOf(row) == 0)
+                {
+                    continue;
+                }
+
+                for (int i = 7; i < row.Keys.Count(); i++)
+                {
+                    List<string> keys = row.Keys.ToList();
+                    string key = keys[i];
+
+                    string name = excelData.First()[key], value = row[key];
+
+                    string areaName = row[Level3DesignationColumn];
+
+                    Specification spec = new Specification()
+                    {
+                        Name = areaName,
+                        IfcVersion = "IFC4",
+                        Description = "Areas ...",
+                        Instructions = "...",
+                        MinOccurs = "0",
+                        MaxOccurs = "unbounded",
+                        Applicability = new Applicability()
+                        {
+                            Entity = new Entity()
+                            {
+                                Name = new Name()
+                                {
+                                    SimpleValue = new SimpleValue()
+                                    {
+                                        Value = "IFCSPACE"
+                                    }
+                                }
+                            }
+                        },
+                        Requirements = new Requirement()
+                        {
+                            Attribute = new Attribute()
+                            {
+                                Instructions = $"For '{areaName}' areas the value of field '{name}' must match '{value}'",
+                                Name = new Name()
+                                {
+                                    SimpleValue = new SimpleValue() { Value = name }
+                                },
+
+                                Value = new Value()
+                                {
+                                    SimpleValue = new SimpleValue() { Value = value }
+                                }
+                            }
+                        }
+                    };
+
+                    specs.Specifications.Add(spec);
+                };
+            }
+
+            return specs;
+        }
+
         public class SpacesSample
         {
             public void Show()
@@ -28,17 +102,19 @@ namespace IDSConverter.IfcConnection
                 using (var model = IfcStore.Open(file))
                 {
                     var project = model.Instances.FirstOrDefault<IIfcProject>();
-                    PrintIfcSpaces(project);
+                    GetIfcSpaces(project);
                 }
             }
 
-            private void PrintIfcSpaces(IIfcProject project)
+            private Dictionary<string,string> GetIfcSpaces(IIfcProject project)
             {
                 var spaces = project.Model.Instances.OfType<IIfcSpace>().ToList();
+                Dictionary<string, string> spaceDictionary = new Dictionary<string, string>();
                 foreach (var space in spaces)
                 {
-                    Console.WriteLine(string.Format("{0} -> {1} [{2}]", space.LongName, space.Name, space.GetType().Name));
+                    spaceDictionary.Add(space.LongName, space.Name);
                 }
+                return spaceDictionary;
             }
         }
 
